80d73c6d4102507048ea3d78b63983a339315421
avanishtatat <avanisht.at.at@gmail.com>
2026-03-28 20:01:04 +0530

    Set up Express server with JSON parsing and CORS support; add health check endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
+require('dotenv').config();
 const express = require("express"); 
+const cors = require("cors"); 
 
+const PORT = process.env.PORT || 3000;
 const app = express(); 
 
-app.listen(3000);
+app.use(express.json()); 
+app.use(cors()); 
+
+// Health Endpoint 
+app.get("/", (req,res) => {
+  res.status(200).json({message: 'Server is running'}); 
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`); 
+});
