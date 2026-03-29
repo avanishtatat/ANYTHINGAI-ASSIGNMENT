8dd4c9e24c66121fb818cb0c3413605d71f88d8f
avanishtatat <avanisht.at.at@gmail.com>
2026-03-29 13:46:29 +0530

    feat: update JWT token generation to use dynamic expiration from environment variable; add Vercel configuration for routing

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -2,5 +2,5 @@ const jwt = require("jsonwebtoken");
 
 // Generate JWT token 
 module.exports.generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN });
 };
